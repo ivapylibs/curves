Fixed derivative in Bezier.py and added optimization

Bezier.py
@@ -1,6 +1,9 @@
-import numpy as np
+#import numpy as np # For basic use
+import jax.numpy as np # For use with optimization
 from scipy.linalg import pascal
 from matplotlib import pyplot as plt
+import pdb
+
 
 class Bezier2D:
     def __init__(self, order):
@@ -21,14 +24,14 @@ class Bezier2D:
 
     def evalJet(self,t):
         vCurve = Bezier2D(self.order-1)
-        vPts = np.diff(self.Q, axis=1)
+        vPts = self.order * np.diff(self.Q, axis=1)
         vCurve.setControlPoints(vPts)
         return vCurve.eval(t)
 
     def evalJet2(self,t):
         aCurve = Bezier2D(self.order-2)
-        vPts = np.diff(self.Q, axis=1)
-        aPts = np.diff(vPts, axis=1)
+        vPts = self.order*np.diff(self.Q, axis=1)
+        aPts = (self.order-1)*np.diff(vPts, axis=1)
         aCurve.setControlPoints(aPts)
         return aCurve.eval(t)
 
@@ -40,10 +43,6 @@ class Bezier2D:
         if(dimension == 1):
             raise RuntimeError('Curvature cannot be calculated for 1D curves')
         elif (dimension == 2):
-            '''
-            _v = np.concatenate((v,np.zeros((1,len(t)))), 0)
-            _a = np.concatenate((a,np.zeros((1,len(t)))), 0)
-            '''
             return np.divide(np.cross(v, a, 0, 0), (np.linalg.norm(v,2,0)**3))
         '''
         case 3 % plot control points in 3D.
@@ -140,4 +139,12 @@ def costFunctionAgree(path):
 
     return Kcurv*totalCurv + Klength*pathLength + Kagree*agree + Kspddev*spddev
 
-def generateCurveParameterization(start, end, d1, d2):
+def generateBezierParam(start, end, d):
+    startDir = start.getRotation()[:,0]
+    endDir = end.getRotation()[:,0]
+    pos2 = start.getTranslation() + startDir * d[0]
+    pos3 = end.getTranslation() - endDir * d[1]
+    b = Bezier2D(3)
+    pts = np.vstack((start.getTranslation(), pos2, pos3, end.getTranslation()))
+    b.setControlPoints(pts.T)
+    return b

optimizeBezier.py
@@ -5,35 +5,59 @@ sys.path.insert(0, pyceres_location)
 
 import PyCeres # Import the Python Bindings
 import numpy as np
+import Bezier
+from Lie import SE2
 from jax import grad
 
 class myCostFunction(PyCeres.CostFunction):
-    def __init__(self):
+    def __init__(self, start, end):
         super().__init__()
+        self.start = start
+        self.end = end
 
         self.set_num_residuals(1)
-        self.set_parameter_block_sizes([1])
+        self.set_parameter_block_sizes([2])
 
     def Evaluate(self, parameters, residuals, jacobians):
-        x=parameters[0][0]
-        costfn = lambda x: 10-x
+        d = np.zeros((2,))
+        d[0] = parameters[0][0]
+        d[1] = parameters[0][1]
 
-        residuals[0] = costfn(x)
-        if(jacobians != None):
-            jacobians[0][0]=grad(costfn)(x)
-        
+        residuals[0] = self.costFromParams(d)
+
+        if (jacobians != None):
+            gradfun = grad(self.costFromParams)
+            gradient = gradfun(d)
+            jacobians[0][0] = gradient[0]
+            jacobians[0][1] = gradient[1]
+
         return True
 
+    def costFromParams(self, d):
+        b = Bezier.generateBezierParam(self.start, self.end, d)
+        return Bezier.costFunctionCurvDev(b)
+
+
+
+start = SE2()
+
+theta = np.pi/2
+R = SE2.rotationMatrix(theta)
+x = np.array([[20], [15]])
+
+end = SE2(R=R, x=x)
+
+initial_guess = [5,5]
+
 # The variable to solve for with its initial value.
-initial_x=5.0
-x=np.array([initial_x]) # Requires the variable to be in a numpy array
+x=np.array(initial_guess) # Requires the variable to be in a numpy array
 
 # Here we create the problem as in normal Ceres
 problem=PyCeres.Problem()
 
 # Creates the CostFunction. This example uses a C++ wrapped function which 
 # returns the Autodiffed cost function used in the C++ example
-cost_function=myCostFunction()
+cost_function=myCostFunction(start, end)
 
 # Add the costfunction and the parameter numpy array to the problem
 problem.AddResidualBlock(cost_function,None,x) 
@@ -47,4 +71,4 @@ summary=PyCeres.Summary()
 # Solve as you would normally
 PyCeres.Solve(options,problem,summary)
 print(summary.BriefReport() + " \n")
-print( "x : " + str(initial_x) + " -> " + str(x) + "\n")
+print( "x : " + str(initial_guess) + " -> " + str(x) + "\n")